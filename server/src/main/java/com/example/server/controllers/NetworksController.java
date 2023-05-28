eae7796e26d8100bd7c58d248dfa4b2ac776a9f2
sem_wett <90400355+semwett0301@users.noreply.github.com>
2023-05-28 22:04:36 +0300

    Add ping endpoint

--- server/src/main/java/com/example/server/controllers/NetworksController.java
+++ server/src/main/java/com/example/server/controllers/NetworksController.java
@@ -2,6 +2,7 @@ package com.example.server.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class NetworksController {
     @GetMapping("/ip")
     public String getUserIP() {
         return request.getRemoteAddr();
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<?> getPing() {
+        return ResponseEntity.ok().build();
     }
 }
