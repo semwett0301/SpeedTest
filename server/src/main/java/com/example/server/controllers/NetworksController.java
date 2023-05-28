c89f9a2506326c64c2b753277b8ea7ce56b7e0e7
sem_wett <90400355+semwett0301@users.noreply.github.com>
2023-05-28 22:01:48 +0300

    Initial commit

---
+++ server/src/main/java/com/example/server/controllers/NetworksController.java
@@ -0,0 +1,19 @@
+package com.example.server.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/network")
+public class NetworksController {
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping("/ip")
+    public String getUserIP() {
+        return request.getRemoteAddr();
+    }
+}
