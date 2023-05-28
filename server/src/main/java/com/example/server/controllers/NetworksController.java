78a409299a3c1cb1b097dca4d6c16ab3e68da386
sem_wett <90400355+semwett0301@users.noreply.github.com>
2023-05-28 23:42:38 +0300

    Add rx and tx endpoints

--- server/src/main/java/com/example/server/controllers/NetworksController.java
+++ server/src/main/java/com/example/server/controllers/NetworksController.java
@@ -3,15 +3,23 @@ package com.example.server.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/network")
 public class NetworksController {
+    private final HttpServletRequest request;
+    private String buffer;
+
     @Autowired
-    private HttpServletRequest request;
+    public NetworksController(HttpServletRequest request) {
+        this.request = request;
+
+        this.buffer = "x";
+        for (var i = 0; i < 23; i++) {
+            this.buffer += this.buffer;
+        }
+    }
 
     @GetMapping("/ip")
     public String getUserIP() {
@@ -22,4 +30,21 @@ public class NetworksController {
     public ResponseEntity<?> getPing() {
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/tx")
+    public ResponseEntity<?> getTx(@RequestBody String buffer) {
+        if (!this.buffer.equals(buffer)) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/rx")
+    public ResponseEntity<String> getRx() {
+        return ResponseEntity
+                .ok()
+                .header("Content-Type", "application/octet-stream")
+                .body(buffer);
+    }
+
+
 }
